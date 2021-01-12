3f52da9e67b866d5d01be3ce0b426a0a77d8c8cb
ariel nowik <newtonis.penguin@gmail.com>
2021-01-12 15:44:44 -0800

    Agragando ejemplo de test de bluetooth

---
+++ bluethootTestProject/src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  providers: [
+    BluetoothSerial
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
